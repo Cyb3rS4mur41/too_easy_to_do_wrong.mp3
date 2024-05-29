41366cced94023766495949044d429e170c7944f
Norbert Szalacsi <104730318+Cyb3rS4mur41@users.noreply.github.com>
2024-05-29 23:16:00 +0200

    Add files via upload

---
+++ agent_smith.ps1
@@ -0,0 +1,10 @@
+# Path to Microsoft Edge executable
+$edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+while ($true) {
+    # Open Microsoft Edge
+    Start-Process $edgePath
+
+    # Wait for 10 seconds before repeating
+    Start-Sleep -Seconds 3
+}
